2bc9b7d204be4dcacebc99abfcffacf1b3cec667
Maxime Baumann <maxime.baumann.pro@gmail.com>
2021-02-09 19:56:20 +0100

    Cours ORM

---
+++ src/MBaumann.Iut.OrmDemo/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace MBaumann.Iut.OrmDemo.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
